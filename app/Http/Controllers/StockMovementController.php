8e0f335238797bf56f5f71173cbd5ea57f28dd18
Fakhri Magang <it.fingerspotmagang9@gmail.com>
2026-03-30 17:31:11 +0700

    membuat migrasi DB  awal

---
+++ app/Http/Controllers/StockMovementController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\stock_movement;
+use Illuminate\Http\Request;
+
+class StockMovementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(stock_movement $stock_movement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(stock_movement $stock_movement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, stock_movement $stock_movement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(stock_movement $stock_movement)
+    {
+        //
+    }
+}
